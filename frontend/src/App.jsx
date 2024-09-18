d056fa0f5f03885567cd6ceb148164feeb43ffa4
Travel85 <31290733+Travel85@users.noreply.github.com>
2024-09-18 20:08:03 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Fragment, useState } from "react";
+import Button from "@mui/material/Button";
+
+//MUI components
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return (
+    <Fragment>
+      <Button variant="contained">Click me!</Button>
+    </Fragment>
+  );
+}
+
+export default App;
