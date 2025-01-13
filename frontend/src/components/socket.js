02ff86584b37c7e578141b275555e8d6e2c432ed
Henry <cyrus.usa@gmail.com>
2025-01-13 17:33:37 +0100

    fixed socket export

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  `${import.meta.env.VITE_BACKEND_URL}` || `${window.origin}`,
-  {
-    path: "/api/session/",
-  }
-);
+export const socket = io(import.meta.env.VITE_BACKEND_URL ?? window.origin, {
+  path: "/api/session/",
+  reconnection: true,
+  reconnectionAttempts: 5,
+  timeout: 10000,
+});
